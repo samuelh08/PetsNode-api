f0234bf8f7e5d7a71898a02e79f6881980539d87
Samuel <samuelh08.shm@gmail.com>
2022-08-07 15:42:19 -0500

    Add mongoose and connect to database

--- server/config/index.js
+++ server/config/index.js
@@ -4,6 +4,11 @@ const config = {
   server: {
     port: process.env.PORT,
   },
+  database: {
+    protocol: process.env.DATABASE_PROTOCOL,
+    url: process.env.DATABASE_URL,
+    username: process.env.DATABASE_PASSWORD,
+  },
 };
 
 module.exports = config;
