6daf552b8abc58f31624d1e91b08c2cd3ebacad9
Samuel <samuelh08.shm@gmail.com>
2022-08-09 19:35:49 -0500

    Add pagination to Pets

--- server/config/index.js
+++ server/config/index.js
@@ -7,7 +7,13 @@ const config = {
   database: {
     protocol: process.env.DATABASE_PROTOCOL,
     url: process.env.DATABASE_URL,
-    username: process.env.DATABASE_PASSWORD,
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+  },
+  pagination: {
+    limit: 10,
+    skip: 0,
+    page: 1,
   },
 };
 
