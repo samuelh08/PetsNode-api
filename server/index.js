f9fe974c95f84a14984d46fe2cfb3b7f28b48a58
Samuel <samuelh08.shm@gmail.com>
2022-08-15 15:18:12 -0500

    Add default CORS

--- server/index.js
+++ server/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const { v4: uuidv4 } = require('uuid');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const logger = require('./config/logger');
 const api = require('./api/v1');
 
 // Init app
 const app = express();
+
+// setup CORS
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    allowedHeaders: ['Accept', 'Content-Type', 'Authorization'],
+  })
+);
 
 // Add unique ID to every request
 app.use((req, res, next) => {
