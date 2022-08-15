af1d8ca9a1de669b84d0e8f661e7c20dea4cbfcb
Samuel <samuelh08.shm@gmail.com>
2022-08-15 14:51:04 -0500

    Add validator for email

--- server/api/v1/users/model.js
+++ server/api/v1/users/model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 const { hash, compare } = require('bcryptjs');
 
 const { Schema } = mongoose;
@@ -22,6 +23,12 @@ const fields = {
     trim: true,
     unique: true,
     lowercase: true,
+    validate: {
+      validator(value) {
+        return validator.isEmail(value);
+      },
+      message: (props) => `${props.value} is not a valid email`,
+    },
   },
   password: {
     type: String,
